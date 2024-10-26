725685c7de7595831c873524392154dfab90d556
03-yash <130440487+03-yash@users.noreply.github.com>
2024-10-26 15:59:44 +0530

    Update index.js

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -35,7 +35,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://finalchat-eight.vercel.app/?vercelToolbarCode=cFp-FwV6IzSjfAU",
     methods: ["GET", "POST"],
   },
 });
